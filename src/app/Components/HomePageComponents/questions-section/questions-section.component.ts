490f173e40ee196b4fc1a532145ca383b871b9d5
DineshCodewents <dinesh.codewents@gmail.com>
2024-02-20 09:42:10 +0530

    Footer Added with responsive.

--- src/app/Components/HomePageComponents/questions-section/questions-section.component.ts
+++ src/app/Components/HomePageComponents/questions-section/questions-section.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./questions-section.component.css']
 })
 export class QuestionsSectionComponent {
+  socialLinks: SocialLink[] = [
+    { iconClass: 'fa-brands fa-linkedin-in', link: 'https://www.linkedin.com/company/ettiksofttechnologies-pvtltd/' },
+    { iconClass: 'fa-brands fa-instagram', link: 'https://www.instagram.com/ettiksoft_technologies?igsh=cnljYzJxYnF2YnU4' },
+    { iconClass: 'fa-brands fa-youtube', link: 'https://youtube.com/@Ettiksoft?si=dOAC3xsdKfbTQUCB' }
+  ];
   
+}
+interface SocialLink {
+  iconClass: string;
+  link: string;
 }
