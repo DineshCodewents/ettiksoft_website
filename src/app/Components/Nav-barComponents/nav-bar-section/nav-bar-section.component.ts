8569e2ba5182f9fef4d0125497cbde9eee72abd5
nandhan-cw <nandhan.codewents@gmail.com>
2024-02-27 11:59:52 +0530

    nav bar changes made

--- src/app/Components/Nav-barComponents/nav-bar-section/nav-bar-section.component.ts
+++ src/app/Components/Nav-barComponents/nav-bar-section/nav-bar-section.component.ts
@@ -1,84 +1,26 @@
-import { Component, HostListener, Renderer2, ElementRef, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
 
+
+import { Component, OnInit } from '@angular/core';
+
+interface NavLink {
+  title: string;
+  links: { title: string }[];
+}
 @Component({
   selector: 'app-nav-bar-section',
   templateUrl: './nav-bar-section.component.html',
   styleUrls: ['./nav-bar-section.component.css']
 })
 export class NavBarSectionComponent implements OnInit{
+  navLinks: NavLink[] = [
+    { title: 'Nav Link 1', links: [
+      { title: 'Link 1.1' }, { title: 'Link 1.2' }, { title: 'Link 1.3' },
+      { title: 'Link 1.4' }, { title: 'Link 1.5' }
+    ]},
+    // Define other NavLinks with their links
+  ];
 
-  showNavBar = false;
-  showMenu = false; // Added variable for menu visibility
-  lastScrollPosition = 0;
+  constructor() { }
 
-  constructor(private renderer: Renderer2, private el: ElementRef, private router: Router) {}
-
-  ngOnInit() {
-    this.addClickOutsideListener();
-    this.addRouterListener();
-  }
-
-  @HostListener('document:scroll')
-  onWindowScroll() {
-    const currentScrollPosition = window.scrollY;
-
-    if (currentScrollPosition > this.lastScrollPosition && currentScrollPosition > 50) {
-      this.showNavBar = false;
-    } else {
-      this.showNavBar = true;
-    }
-
-    this.lastScrollPosition = currentScrollPosition;
-  }
-
-  toggleMenu() {
-    this.showMenu = !this.showMenu;
-    this.toggleHamburgerIcon();
-
-    // Add logic to change background color when the menu is toggled
-    const navBar = this.el.nativeElement.querySelector('.nav-bar');
-    if (this.showMenu) {
-      this.renderer.setStyle(navBar, 'background-color', 'black');
-    } else {
-      this.renderer.removeStyle(navBar, 'background-color');
-    }
-  }
-
-  private toggleHamburgerIcon() {
-    const navIcon = this.el.nativeElement.querySelector('#nav-icon1');
-    this.renderer.removeClass(navIcon, 'open');
-    if (this.showMenu) {
-      this.renderer.addClass(navIcon, 'open');
-    }
-  }
-
-  private addClickOutsideListener() {
-    document.addEventListener('click', (event) => {
-      if (!this.el.nativeElement.contains(event.target)) {
-        // Clicked outside the nav-bar, close the menu
-        this.showMenu = false;
-        this.toggleHamburgerIcon();
-
-        // Reset background color when closing the menu
-        const navBar = this.el.nativeElement.querySelector('.nav-bar');
-        this.renderer.removeStyle(navBar, 'background-color');
-      }
-    });
-  }
-
-  private addRouterListener() {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        // Close the menu when navigating to another page
-        this.showMenu = false;
-        this.toggleHamburgerIcon();
-
-        // Reset background color when closing the menu
-        const navBar = this.el.nativeElement.querySelector('.nav-bar');
-        this.renderer.removeStyle(navBar, 'background-color');
-      });
-  }
+  ngOnInit() { }
 }
