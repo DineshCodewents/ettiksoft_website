1f978b20177078692fe1dd109a3d84356180bc37
nandhan-cw <nandhan.codewents@gmail.com>
2024-02-28 13:55:06 +0530

    our leaders done

--- src/app/Components/HomePageComponents/our-leaders-section/our-leaders-section.component.ts
+++ src/app/Components/HomePageComponents/our-leaders-section/our-leaders-section.component.ts
@@ -6,6 +6,8 @@ interface LeaderCard {
   name: string;
   position: string;
   company: string;
+  path_f:string;
+  path_b:string;
   flipped: boolean;
   backContent: {
     quoteIcon: string;
@@ -28,27 +30,36 @@ export class OurLeadersSectionComponent {
       backContent: {
         quoteIcon: 'fa-solid fa-quote-left',
         description: 'At our company, we foster a culture of innovation and collaboration, empowering every employee to contribute their best ideas and talents. Together, we\'re not just shaping the future; we\'re redefining it.'
-      }
+      },
+      path_f: 'kk-f',
+      path_b: 'kk-b',
+      
     },
     { 
-      name: '-Carmelo Messina',
-      position: 'General Manager',
+      name: 'Carmelo Messina',
+      position: 'General Manager, Europe',
       company: 'ETTIKSOFT, Europe',
       flipped: false,
       backContent: {
         quoteIcon: 'fa-solid fa-quote-left',
         description: 'At our company, we foster a culture of innovation and collaboration, empowering every employee to contribute their best ideas and talents. Together, we\'re not just shaping the future; we\'re redefining it.'
-      }
+      },
+      path_f: 'melo-f',
+      path_b: 'melo-b',
+      
     },
     { 
-      name: '-Chidamparam Velusamy',
+      name: 'Chidamparam Velusamy',
       position: 'CEO',
       company: 'ETTIKSOFT',
       flipped: false,
       backContent: {
         quoteIcon: 'fa-solid fa-quote-left',
         description: 'At our company, we foster a culture of innovation and collaboration, empowering every employee to contribute their best ideas and talents. Together, we\'re not just shaping the future; we\'re redefining it.'
-      }
+      },
+      path_f: 'chidambaram-f',
+      path_b: 'chidambaram-b',
+      
     },
     // Add more cards as needed
   ];
